01613ac3a75b89f5d59c076ecafe9f634949c073
Jett Kaspar <spmaamil@gmail.com>
2017-10-27 12:30:06 -0500

    Changed some stuff in bspwm so connman would appear correctly

--- copy.sh
+++ copy.sh
@@ -5,7 +5,6 @@ cp -r ~/.config/polybar ~/Archrice/.config
 cp  ~/.config/bspwm/bspwmrc ~/ArchRice/.config/bspwm/bspwmrc
 cp  -r ~/.config/cava ~/ArchRice/.config/cava
 cp  ~/.config/sxhkd/sxhkdrc ~/ArchRice/.config/sxhkd/sxhkdrc
-cp  -r ~/.config/mopidy ~/ArchRice/.config/mopidy
 cp  -r ~/.config/mpv ~/ArchRice/.config/mpv
 cp  -r ~/.config/ranger ~/ArchRice/.config/ranger
 

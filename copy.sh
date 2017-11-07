2ffbaf4d0bc0037b70e07d2a63f4a255606183cb
Jett Kaspar <spmaamil@gmail.com>
2017-11-06 18:22:25 -0600

    I LIKE CODE STUFF

--- copy.sh
+++ copy.sh
@@ -2,12 +2,12 @@
 
 cp -r ~/Pictures/Wallpapers ~/Archrice
 cp -r ~/.config/polybar ~/Archrice/.config
-cp  ~/.config/bspwm/bspwmrc ~/ArchRice/.config/bspwm/bspwmrc
-cp  -r ~/.config/cava ~/ArchRice/.config/cava
-cp  ~/.config/sxhkd/sxhkdrc ~/ArchRice/.config/sxhkd/sxhkdrc
-cp  -r ~/.config/mpv ~/ArchRice/.config/mpv
-cp  -r ~/.config/ranger ~/ArchRice/.config/ranger
-
+cp ~/.config/bspwm/bspwmrc ~/ArchRice/.config/bspwm/bspwmrc
+cp -r ~/.config/cava ~/ArchRice/.config/cava
+cp ~/.config/sxhkd/sxhkdrc ~/ArchRice/.config/sxhkd/sxhkdrc
+cp -r ~/.config/mpv ~/ArchRice/.config/mpv
+cp -r ~/.config/ranger ~/ArchRice/.config/ranger
+cp ~/.config/rofi/config.rasi ~/ArchRice/.config/rofi 
 
 git add .
 OUTPUT="$(git status)"

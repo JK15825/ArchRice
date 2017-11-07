b7f8111b93814d7885b1a782148661ed1f4ed2e6
Jett Kaspar <spmaamil@gmail.com>
2017-11-07 15:36:07 -0600

    The window thing works now.

--- .config/polybar/windows.py
+++ .config/polybar/windows.py
@@ -2,20 +2,20 @@
 import subprocess 
 temp =  subprocess.Popen("xlsclients",stdout=subprocess.PIPE).stdout.read()
 temp = temp.decode("utf-8")
-temp = temp.replace('\n','').replace("arch_book","").replace("  ","", 1).replace("/usr/lib/firefox/firefox  ","");
+temp = temp.replace('\n','').replace("arch_book","").replace("  ","", 1).replace("/usr/lib/firefox/firefox","");
 temp = temp.split("  ");
 dict = {}
 for val in temp:
     val = val[0:1].upper() + val[1:]
-    if(dict.get(val,-1) == -1):
+    if(len(val) > 0 and dict.get(val,-1) == -1):
         dict.update({val:1})
-    else:
+    elif(len(val) > 0):
         dict[val] = dict[val] + 1
-toprint = ""
+toprint = " | "
 for val in dict:
     if(dict[val] == 1):
         toprint += val + " | "
     else:
         toprint += val + " " + str(dict[val]) + " | "
 
-print(toprint[0:len(toprint) - 2] + "  " )
+print(toprint)
